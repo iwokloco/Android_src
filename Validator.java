eb3a625ac4a164e3c8ad25b5d4e2e3ad4c5bb460
iwokloco <iwokloco@gmail.com>
2015-03-13 12:32:31 +0100

    Validator
    This class validates the format of phones and emails.

---
+++ Validator.java
@@ -0,0 +1,22 @@
+package iwok.com.utils;
+
+import android.text.TextUtils;
+import android.util.Patterns;
+
+public class Validator{
+
+    public static boolean isValidEmail(CharSequence email) {
+        if (!TextUtils.isEmpty(email)) {
+            return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+        }
+        return false;
+    }
+
+    public static boolean isValidPhoneNumber(CharSequence phoneNumber) {
+        if (!TextUtils.isEmpty(phoneNumber)) {
+            return Patterns.PHONE.matcher(phoneNumber).matches();
+        }
+        return false;
+    }
+
+}
